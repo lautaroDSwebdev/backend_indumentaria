d416feecf6f5b52e63f061719f62311f764f0b9b
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-05-15 03:33:39 -0300

    delete exception

--- src/main/java/com/example/demo/exceptions/BodyNotValidException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exceptions;
-
-public class BodyNotValidException extends RuntimeException {
-
-    public BodyNotValidException(String message) {
-        super(message);
-    }
-
-}
